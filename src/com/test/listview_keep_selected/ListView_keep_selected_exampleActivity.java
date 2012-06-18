5b646004009899f68f5aea5199395c8d61f14bec
Andrzej Gis <xaphones@gmail.com>
2012-06-18 14:09:38 +0200

    first commit

---
+++ src/com/test/listview_keep_selected/ListView_keep_selected_exampleActivity.java
@@ -0,0 +1,24 @@
+package com.test.listview_keep_selected;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ListView_keep_selected_exampleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        ListView lv = (ListView)findViewById(R.id.listViewTest);
+        lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        
+        String[] stringArray = {"aaaa", "bbbb", "cccc", "ddd", "eee"};
+        
+        lv.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.list_item, stringArray));
+
+    }
+}
